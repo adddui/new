36806ca256f3035e13387674990b7a0c58131df7
uer <sityhitty@gmail.com>
2022-08-28 10:04:01 +0800

    config

---
+++ src/main/java/com/example/project/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.example.project.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 通过该配置类实现swaggerg工具的注入
+ * 通过一些注解，让类能够被框架管理，被使用
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    //基本信息的配置，信息会在api文档上显示
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("接口文档")
+                .description("demo的接口的文档")
+                .termsOfServiceUrl("http://localhost:8080/hello")
+                .version("1.0")
+                .build();
+    }
+}
