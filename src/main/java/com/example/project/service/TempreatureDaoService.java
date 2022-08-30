c79f14684647378c30999a2529dbee51d7e1bfd4
2669615250 <106328781+2669615250@users.noreply.github.com>
2022-08-30 16:50:04 +0800

    Add files via upload

---
+++ src/main/java/com/example/project/service/TempreatureDaoService.java
@@ -0,0 +1,23 @@
+package com.example.project.service;
+import com.example.project.entity.Tempreature;
+import com.example.project.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+
+public interface TempreatureDaoService {
+
+        List<Tempreature> findAll();
+        //新增数据
+        int add(Tempreature tempreature);
+        //删除数据
+        Integer delete(int id);
+        //根据id查找
+        Tempreature findTempreatureById(int id);
+        //根据name查找
+        Tempreature findTempreatureByName(String name);
+        //更新数据
+        int update(Tempreature user);
+}
