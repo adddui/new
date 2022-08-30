78926e58912930d7016615dbdd94408d12691d59
kleene123 <107232643+kleene123@users.noreply.github.com>
2022-08-30 10:52:04 +0800

    organ和tempreature

---
+++ 1/1/Tempreature.java
@@ -0,0 +1,17 @@
+package com.example.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tempreature {
+
+    int id;
+    int uid ;
+    int temp;
+    String postTime;
+
+}
