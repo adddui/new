fd305b03c7e9e27c9fc9d8189fecf66e568805dd
2669615250 <106328781+2669615250@users.noreply.github.com>
2022-08-30 16:49:09 +0800

    Add files via upload

---
+++ src/main/java/com/example/project/entity/Organ_info.java
@@ -0,0 +1,15 @@
+package com.example.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Organ_info {
+    int id;
+    String organName;
+    String address;
+}
